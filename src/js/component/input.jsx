cfe15c381cb593aff4a32893c94ba3e5a08aaf13
Rocio <144937216+Rociosantos18@users.noreply.github.com>
2024-01-16 18:05:22 +0000

    last commit

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -15,29 +15,31 @@ const Input = () => {
         }    
 
     const [list, setList] = useState([]);
+    const handleOnKeyDown = (e) => {
+        if (e.key === "Enter") {
+            validateInput()
+            setInputValue("")
+        };
+    
+    }
     return (
         <div className="body">
             <h1 className="text-center mt-5"> To do list</h1>
             <ul className="list-group list-group-flush">
                 <li>
                     <input type="text" onChange={e => validate(e.target.value)} value={inputValue} placeholder="What needs to be done?"
-                        onKeyPress={(e) => {
-                            if (e.key === "Enter") {
-                                validateInput()
-                                setInputValue("")
-                            };
-                        }} />
+                        onKeyDown={handleOnKeyDown} />
                 </li>
                 {list.map((element,index) => (
                     <li className="element list-group-item d-flex justify-content-between align-items-center">
                        {element}
                        <i className="close fa-regular fa-circle-xmark" 
                        onClick={()=> 
-                        setList(list.filter((t,currentIndex) =>index != currentIndex))}></i>
+                        setList(list.filter((_,currentIndex) =>index != currentIndex))}></i>
                     </li>))
                 
                 }
-                <li className="number list-group-item">{list.length} item left</li>
+                <li className="number list-group-item">{list.length === 0 ? "No tasks, add new task" : `${list.length} task${list.length === 1 ? '' : 's' }` } </li>
             </ul>
             
         </div>
